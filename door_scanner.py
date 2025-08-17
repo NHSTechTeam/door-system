a7252f1b532cfa381b08f7d7972c5b5946fdb464
Andrew Smiley <76449307+Pinball3D@users.noreply.github.com>
2025-08-17 16:19:34 -0400

    Base

---
+++ door_scanner.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import paho.mqtt.client as mqtt
+import evdev
+import time
+import subprocess
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MQTT_BROKER = os.getenv("MQTT_BROKER")
+MQTT_PORT =  os.getenv("MQTT_PORT")
+MQTT_USERNAME = os.getenv("MQTT_USERNAME")
+MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
+MQTT_TOPIC_UNLOCK = "door/unlock"
+MQTT_TOPIC_SCAN = "door/scan"
+
+
+def find_keyboard():
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    for dev in devices:
+        if "Barcode" in dev.name.lower() or "Keyboard" in dev.name.lower():
+            return dev
+    raise RuntimeError("No barcode scanner found")
+
+
+def unlock_door():
+    try:
+        print("Unlocking door...")
+        subprocess.run(["hidrelay", "--on", "1"], check=True)
+        time.sleep(3)
+        subprocess.run(["hidrelay", "--off", "1"], check=True)
+    except Exception as e:
+        print(f"Error unlocking door: {e}")
+
+
+def read_scanner(device, client):
+    input_text = ""
+    for event in device.read_loop():
+        if event.type == evdev.ecodes.EV_KEY:
+            key = evdev.categorize(event)
+            if key.keystate == 1:  # key down
+                if key.keycode == "KEY_ENTER":
+                    if input_text:
+                        payload = {"type": "barcode", "code": input_text}
+                        client.publish(MQTT_TOPIC_SCAN, json.dumps(payload))
+                        input_text = ""
+                else:
+                    code = key.keycode.replace("KEY_", "")
+                    if len(code) == 1:
+                        input_text += code
+        
+
+
+def on_message(client, userdata, msg):
+    if msg.topic == MQTT_TOPIC_UNLOCK:
+        unlock_door()
+
+
+client = mqtt.Client()
+client.on_message = on_message
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+client.connect(MQTT_BROKER, MQTT_PORT, 60)
+client.subscribe(MQTT_TOPIC_UNLOCK)
+client.loop_start()
+
+read_scanner(find_keyboard(), client)
