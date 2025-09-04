882a943a8c865a1e1844639d5dfe45ff58a25933
Andrew Smiley <76449307+Pinball3D@users.noreply.github.com>
2025-09-04 19:16:07 -0400

    install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+$dir = "/opt/NHSTechTeam/door-system"
+
+apt update && apt install -y git python
+git clone https://github.com/NHSTechTeam/door-system.git $dir
+cd $dir
+pip install -r requirements.txt
+cp .env.example .env
+
+sudo mv door-scanner.service /etc/systemd/system/door-scanner.service
+sudo systemctl daemon-reload
+sudo systemctl enable door-scanner.service
+sudo systemctl start door-scanner.service
+
+echo "Installation complete. Please edit the .env file with your configuration."
