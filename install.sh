bb9a5ec6a6b6568d737861b0263a846801457766
Andrew Smiley <76449307+Pinball3D@users.noreply.github.com>
2025-09-26 17:48:27 -0400

    mistake

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ sudo apt update && apt install -y git python3 python3-pip usbrelay xinit openbox
 git clone https://github.com/NHSTechTeam/door-system.git $dir
 cd $dir
 pip install -r requirements.txt --break-system-packages
-sudo chmod +x door_scanner.py
+sudo chmod +x door-scanner.py
 cp .env.example .env
 
 sudo mv door-scanner.service /etc/systemd/system/door-scanner.service
