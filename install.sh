537c533e6b9bcc6d4eb0900a2e065535b3e4f2f8
Andrew Smiley <76449307+Pinball3D@users.noreply.github.com>
2025-09-28 20:53:22 -0400

    readability changes

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 dir="/opt/NHSTechTeam/door-system"
 tmpdir="/tmp/nhstt"
 
+echo "-- NHS Tech Team Door System Installer --"
+
 #move env if it exists
 if [ -d "$dir" ]; then
-  echo "Directory already exists. Copying .env and re installing."
+  echo " --> Directory already exists. Copying .env and re installing."
   sudo mkdir -p $tmpdir
   sudo cp $dir/.env $tmpdir/.env
   sudo rm -rf $dir
 fi
 
+echo " --> Installing libraries and dependencies..."
 sudo apt update && apt install -y git python3 python3-pip usbrelay xinit openbox chromium-browser
 git clone https://github.com/NHSTechTeam/door-system.git $dir
 cd $dir
@@ -16,6 +19,7 @@ pip install -r requirements.txt --break-system-packages
 sudo chmod +x door-scanner.py
 cp .env.example .env
 
+echo " --> Setting up services..."
 sudo mv door-scanner.service /etc/systemd/system/door-scanner.service
 sudo mv door-gui.service /etc/systemd/system/door-gui.service
 sudo systemctl daemon-reload
@@ -26,6 +30,7 @@ sudo systemctl start door-gui
 
 
 #auto login user on boot
+echo " --> Setting up auto login on tty1"
 sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf > /dev/null <<EOF
 [Service]
 ExecStart=
@@ -33,6 +38,7 @@ ExecStart=-/sbin/agetty --autologin $(logname) --noclear %I \$TERM
 EOF
 
 #auto start gui on boot
+echo " --> Creating the autostart file"
 sudo cp .bash_profile ~/.bash_profile
 
 
@@ -40,7 +46,7 @@ sudo cp .bash_profile ~/.bash_profile
 if [ -d "$tmpdir/.env" ]; then
   sudo cp $tmpdir/.env $dir/.env
   sudo rm $tmpdir/.env
-  echo "Reinstallation complete. Your previous .env file has been restored."
+  echo " --> Reinstallation complete. Your previous .env file has been restored."
 else
-    echo "Installation complete. Please edit the .env file with your configuration."
+    echo " --> Installation complete. Please edit the .env file with your configuration."
 fi
