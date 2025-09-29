d9926bf5a72ab7229cbd6274223235ad580c9292
Andrew Smiley <76449307+Pinball3D@users.noreply.github.com>
2025-09-28 21:41:35 -0400

    fix

--- install.sh
+++ install.sh
@@ -21,12 +21,9 @@ cp .env.example .env
 
 echo " --> Setting up services..."
 sudo mv door-scanner.service /etc/systemd/system/door-scanner.service
-sudo mv door-gui.service /etc/systemd/system/door-gui.service
 sudo systemctl daemon-reload
 sudo systemctl enable door-scanner
-sudo systemctl enable door-gui
 sudo systemctl start door-scanner
-sudo systemctl start door-gui
 
 
 #auto login user on boot
